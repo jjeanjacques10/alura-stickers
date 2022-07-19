16c44ccb239819d021d3a0737be804ecd7c45ebf
jjeanjacques10 <jjean.jacques10@gmail.com>
2022-07-18 22:43:17 -0300

    imdb movies

---
+++ README.md
@@ -0,0 +1,21 @@
+## Imersão Java Alura
+
+Projeto da Imersão Java da Alura.
+
+Site da Imersão: <https://www.alura.com.br/imersao-java>
+
+## Estrucura do projeto
+
+- `src`: Arquivos de código fonte
+  - `movies`: Código fonte do App que chama o IMDb API
+
+## IMDb API
+
+Site de consulta de filmes e séries. Estamos utilizando a API pública deles para obter os dados.
+
+URL da API: <https://imdb-api.com/>
+
+- GET <https://imdb-api.com/en/API/Top250Movies/>
+
+---
+Developed by [@jjeanjacques10](https://twitter.com/jjeanjacques10)
---
+++ src/movies/App.java
@@ -0,0 +1,34 @@
+package movies;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String API_KEY = args[0];
+        String url = "https://imdb-api.com/en/API/Top250Movies/" + API_KEY;
+
+        URI endpoint = URI.create(url);
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder(endpoint).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+        String body = response.body();
+        System.out.println(body);
+
+        // Extrair apenas os dados que interessam (título, poster, classificação, ano)
+        List<Map<String, String>> movies = JsonParser.parse(body);
+
+        // Exibir e manibular os dados
+        movies.forEach(m -> {
+            System.out.println(m);
+        });
+    }
+}
+// #endregion
